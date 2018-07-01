3f2fed85f37b574c14b72766ce25a7c89796f4b9
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-01 21:20:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,11 @@
 
 class Song
   attr_accessor :name, :artist, :genre
+  @@all = []
 
   def initialize(name, artist, genre)
-    @name = name 
+    @name = name
     @artist = artist
-    @genre = genre 
+    @genre = genre
   end
 end
