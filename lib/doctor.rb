36b9fc3af5c0f9b0e8309b11ce2fa72cc0d93a0f
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-01 21:58:09 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,8 +10,8 @@ class Doctor
     @@all << self
   end
 
-  def new_appointment
-
+  def new_appointment(patient, date)
+    appointment = Appointment.new(patient, self, date)
   end
 
   def patients
